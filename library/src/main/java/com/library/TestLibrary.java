a2b48da17f794e0f6c6d1575b1fe21d16ca47cbf
chen_fulei <cfulei@gmail.com>
2015-07-15 11:35:23 +0800

    Rejected commit: Default

---
+++ library/src/main/java/com/library/TestLibrary.java
@@ -0,0 +1,14 @@
+package com.library;
+
+import android.util.Log;
+
+/**
+ * Created by chen_fulei on 2015/7/15.
+ */
+public class TestLibrary {
+
+    public static void first(){
+        Log.e("" , "this is test!");
+    }
+
+}
