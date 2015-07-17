b3c1ab19487ec66f4ab7443ee2d06ebf079846ef
chen_fulei <cfulei@gmail.com>
2015-07-17 14:09:06 +0800

    Add File

--- library/src/main/java/com/library/ui/FLActivityManager.java
+++ library/src/main/java/com/library/ui/FLActivityManager.java
@@ -1,5 +1,7 @@
 package com.library.ui;
 
+import android.app.Activity;
+
 import java.util.Stack;
 
 /**
@@ -45,5 +47,91 @@ public class FLActivityManager {
         return flStack.size();
     }
 
+    /**
+     * 添加activity到堆栈里
+     */
+    public void add(FLBaseAct act) {
+        if (flStack == null) {
+            flStack = new Stack<>();
+        }
+
+        flStack.add(act);
+    }
+
+    /**
+     * 暂停堆栈最顶部的activity
+     *
+     * @return
+     */
+    public Activity stop() {
+        if (flStack == null) {
+            throw new NullPointerException("Activity Stack is null,Activity must extend HCACtivity");
+        }
+
+        if (flStack.isEmpty()) {
+            return null;
+        }
+
+        return (Activity) flStack.lastElement();
+    }
+
+    /**
+     * 结束当前的activity (在堆栈最顶部)
+     */
+    public void finish() {
+        FLBaseAct act = flStack.lastElement();
+        finish((Activity) act);
+    }
+
+    /**
+     * 指定结束的activity
+     *
+     * @param activity
+     */
+    public void finish(Activity activity) {
+        if (activity != null) {
+            flStack.remove(activity);
+
+            activity = null;
+        }
+    }
+
+    /**
+     * 指定结束activity
+     *
+     * @param cls
+     */
+    public void finish(Class<?> cls) {
+        for (FLBaseAct act : flStack) {
+            if (act.getClass().equals(cls)) {
+                finish((Activity) act);
+            }
+        }
+    }
+
+    /**
+     * 结束所有activity
+     */
+    public void finishAll() {
+        for (int i = 0; i < flStack.size(); i++) {
+            if (null != flStack.get(i)) {
+                ((Activity) flStack.get(i)).finish();
+            }
+        }
+
+        flStack.clear();
+    }
+
+    /**
+     * 结束整个app
+     */
+    public void appExit() {
+        try {
+            finishAll();
+            Runtime.getRuntime().exit(0);
+        } catch (Exception e) {
+            Runtime.getRuntime().exit(-1);
+        }
+    }
 
 }
---
+++ library/src/main/java/com/library/util/Debug.java
@@ -0,0 +1,80 @@
+package com.library.util;
+
+import android.util.Log;
+
+/**
+ * 全局的debug管理
+ * <p/>
+ * Created by chen_fulei on 2015/7/17.
+ */
+public class Debug {
+
+    private static boolean DEBUG = true;
+
+    /**
+     * 设置debug开关
+     *
+     * @param enable
+     */
+    public static void setEnableDebug(boolean enable) {
+        DEBUG = enable;
+    }
+
+    /**
+     * 默认打印logcat  Info
+     *
+     * @param message logcat 信息
+     */
+    public static void Log(String message) {
+        Log("", message);
+    }
+
+    public static void Log(String tag, String message) {
+        if (DEBUG) {
+            Log.i(tag, message);
+        }
+    }
+
+    /**
+     * 打印异常错误信息
+     *
+     * @param e
+     */
+    public static void Log(Throwable e) {
+        LogW(Log.getStackTraceString(e));
+    }
+
+    public static void Log(String tag, Throwable e) {
+        LogW(tag, Log.getStackTraceString(e));
+    }
+
+    /**
+     * 打印错误信息 Error
+     *
+     * @param message
+     */
+    public static void LogE(String message) {
+        LogE("", message);
+    }
+
+    public static void LogE(String tag, String message) {
+        if (DEBUG) {
+            Log.e(tag, message);
+        }
+    }
+
+    /**
+     * 打印wrong logcat
+     *
+     * @param message
+     */
+    public static void LogW(String message) {
+        LogW("", message);
+    }
+
+    public static void LogW(String tag, String message) {
+        if (DEBUG) {
+            Log.w(tag, message);
+        }
+    }
+}
